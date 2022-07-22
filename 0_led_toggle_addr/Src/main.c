0c3e95ba1ed057bd1116e3d46c174fa9913a9278
stasxplay <stas.jurgensons@gmail.com>
2022-07-22 07:30:55 +0300

    changes

--- 0_led_toggle_addr/Src/main.c
+++ 0_led_toggle_addr/Src/main.c
@@ -7,16 +7,25 @@
 #define APB2PERIPH_BASE		(PERIPH_BASE + APB2PERIPH_OFFSET)
 #define GPIOA_OFFSET  		(0x00000800UL)
 #define GPIOA_BASE			(APB2PERIPH_BASE + GPIOA_OFFSET)
-#define RCC_APB2EN_R 		(0x18UL)
+#define RCC_APB2EN_R 		(*(volatile unsigned int *)(0x18UL))
 #define MODE_R_OFFSET 		(0x00UL)
-#define GPIOA_MODE_R 		(GPIOA_BASE + MODE_R_OFFSET)
+#define GPIOA_MODE_R 		(*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET))
 #define OD_R_OFFSET 		(0x0C)
-#define GPIOA_OD_R 			(GPIOA_BASE + OD_R_OFFSET)
+#define GPIOA_OD_R 			(*(volatile unsigned int *)(GPIOA_BASE + OD_R_OFFSET))
 
 #define IOPAEN (1U<<2)
 
 #define PIN1 (1U<<1)
-/*
- * (1U<<4)
- * &=~(1U<<5)
- * */
+
+
+int main(void){
+
+	RCC_APB2EN_R |= IOPAEN;
+
+	GPIOA_MODE_R |=  (1U<<4);
+	GPIOA_MODE_R &=~ (1U<<5);
+
+	while (1){
+		GPIOA_OD_R |= PIN1;
+	}
+}
