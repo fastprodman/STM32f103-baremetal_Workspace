4adcf7ed1ef391184edd93446ed29a4d8230d387
stasxplay <stas.jurgensons@gmail.com>
2022-07-29 02:34:42 +0300

    rx

--- 5_uart_tx/Src/main.c
+++ 5_uart_tx/Src/main.c
@@ -6,7 +6,7 @@
 #define CR1_TE			(1U<<3)
 #define CR1_UE			(1U<<13)
 #define SR_TXE			(1U<<7)
-#define SYS_FREQ		16000000
+#define SYS_FREQ		8000000
 #define APB1_CLK		SYS_FREQ
 #define UART_BAUDRATE	115200
 
@@ -40,20 +40,18 @@ void uart2_tx_init(void){
 	//Configure uart boudrate
 	uart_set_baudrate(USART2, APB1_CLK, UART_BAUDRATE);
 	//configure transfer direction
-	USART2->CR1 = CR1_TE; // Transmitter enable
-	//enable uart module
-	USART2->CR1 = CR1_UE; //Enable USART
+	USART2->CR1 = CR1_UE | CR1_TE; // Transmitter enable, Enable USART
 }
 
 void uart2_write(int ch){
 	//Make sure transmit data register is empty
-	while(!(USART2->SR & SR_TXE)){}
+	while(!(USART2->SR & SR_TXE));
 	//Write to transmit data register
 	USART2->DR = (ch & 0xFF);
 }
 
-static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BoudRate){
-	USARTx->BRR = compute_uart_div(PeriphClk, BoudRate);
+static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate){
+	USARTx->BRR = compute_uart_div(PeriphClk, BaudRate);
 }
 
 static uint16_t compute_uart_div(uint32_t PeriphClk, uint32_t BaudRate){
---
+++ 7_uart_modilar/Inc/uart.h
@@ -0,0 +1,8 @@
+#ifndef UART_H_
+#define UART_H_
+#include "stm32f1xx.h"
+#include <stdint.h>
+
+void uart2_tx_init(void);
+
+#endif /* UART_H_ */
